6582b2a8f2618ce3865ddeb7d27c3c09379055aa
Aziz-dev22 <testnetmelli@gmail.com>
2026-06-28 07:05:57 +0330

    Set up database and include API router
    Create database tables and include API router.

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,19 +2,18 @@ from fastapi import FastAPI
 
 from app.api.v1.router import router
 from app.core.config import settings
+from app.db.database import Base, engine
 
 app = FastAPI(
     title=settings.APP_NAME
 )
 
-app.include_router(
-    router,
-    prefix="/api/v1"
-)
+# ساخت جدول‌ها
+Base.metadata.create_all(bind=engine)
+
+app.include_router(router, prefix="/api/v1")
 
 
 @app.get("/")
 async def root():
-    return {
-        "message": "Atlas VPN Backend Running"
-    }
+    return {"message": "Atlas VPN Backend Running"}
