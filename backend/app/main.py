6741041bba85a414f47c1ca9303621f538157182
Aziz-dev22 <testnetmelli@gmail.com>
2026-06-28 06:44:58 +0330

    Refactor FastAPI app initialization and routing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.api.v1.router import router
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.APP_NAME
+)
+
+app.include_router(
+    router,
+    prefix="/api/v1"
+)
 
 
 @app.get("/")
-def root():
-    return {"message": "Atlas VPN Backend Running"}
+async def root():
+    return {
+        "message": "Atlas VPN Backend Running"
+    }
