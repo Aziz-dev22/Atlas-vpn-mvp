4ad79c1a62632f6e63359fe37ecc7036c5c7fbad
Aziz-dev22 <testnetmelli@gmail.com>
2026-06-28 06:57:56 +0330

    Implement JWT access token creation function

---
+++ backend/app/core/security.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+from jose import jwt
+
+from app.core.config import settings
+
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24  # 1 day
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
