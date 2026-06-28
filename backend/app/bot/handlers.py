894436eafd6b6f883e95b942196ed43866f76fb5
Aziz-dev22 <testnetmelli@gmail.com>
2026-06-28 07:34:49 +0330

    Implement user login and token storage
    Added user login functionality and token management.

--- backend/app/bot/handlers.py
+++ backend/app/bot/handlers.py
@@ -1,21 +1,32 @@
 from aiogram import Router, types, F
 from aiogram.types import CallbackQuery
 
-from app.bot.client import get_plans, buy_plan
+from app.bot.client import login_user, get_plans, buy_plan
 from app.bot.keyboards import plans_keyboard
 
 router = Router()
 
+# حافظه ساده (بعداً Redis می‌کنیم)
+user_tokens = {}
+
 
 @router.message()
 async def start(message: types.Message):
+
+    telegram_id = str(message.from_user.id)
+
+    login = await login_user(telegram_id)
+
+    token = login["access_token"]
+
+    user_tokens[telegram_id] = token
 
     plans = await get_plans()
 
     keyboard = plans_keyboard(plans)
 
     await message.answer(
-        "💎 پلن‌های موجود:\n\nبرای خرید یکی را انتخاب کن:",
+        "💎 پلن‌های موجود:\n\nبرای خرید انتخاب کن:",
         reply_markup=keyboard
     )
 
@@ -23,12 +34,16 @@ async def start(message: types.Message):
 @router.callback_query(F.data.startswith("buy_"))
 async def buy(callback: CallbackQuery):
 
+    telegram_id = str(callback.from_user.id)
     plan_id = int(callback.data.split("_")[1])
 
-    result = await buy_plan(
-        plan_id=plan_id,
-        telegram_id=str(callback.from_user.id)
-    )
+    token = user_tokens.get(telegram_id)
+
+    if not token:
+        await callback.message.answer("خطا: دوباره /start بزن")
+        return
+
+    result = await buy_plan(plan_id, token)
 
     await callback.message.answer(
         f"✅ خرید موفق!\n\n"
