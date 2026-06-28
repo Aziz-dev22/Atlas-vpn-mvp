02ef4b2e5fe3431d6b2c419798865d71a5f900fb
Aziz-dev22 <testnetmelli@gmail.com>
2026-06-28 07:30:46 +0330

    Refactor user functions to get plans and buy plan

--- backend/app/bot/client.py
+++ backend/app/bot/client.py
@@ -4,10 +4,18 @@ import httpx
 BASE_URL = "http://backend:8000/api/v1"
 
 
-async def create_or_get_user(user_data: dict):
+async def get_plans():
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{BASE_URL}/plans")
+        return response.json()
+
+
+async def buy_plan(plan_id: int, telegram_id: str):
     async with httpx.AsyncClient() as client:
         response = await client.post(
-            f"{BASE_URL}/users",
-            json=user_data
+            f"{BASE_URL}/buy/{plan_id}",
+            headers={
+                "Authorization": f"Bearer {telegram_id}"
+            }
         )
         return response.json()
