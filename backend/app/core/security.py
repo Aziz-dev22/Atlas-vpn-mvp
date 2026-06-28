8ba9aefd4fc70c522c9940ec1ca54b38a2862633
Aziz-dev22 <testnetmelli@gmail.com>
2026-06-28 07:10:09 +0330

    Refactor create_access_token function

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -3,9 +3,8 @@ from jose import jwt
 
 from app.core.config import settings
 
-
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24  # 1 day
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
 
 def create_access_token(data: dict):
@@ -14,10 +13,8 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(
+    return jwt.encode(
         to_encode,
         settings.SECRET_KEY,
         algorithm=ALGORITHM
     )
-
-    return encoded_jwt
