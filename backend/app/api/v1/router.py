4fc012a9c97071f2e16d2a6f2c8bd73a26a7ec76
Aziz-dev22 <testnetmelli@gmail.com>
2026-06-28 07:21:06 +0330

    Update router.py

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -4,6 +4,7 @@ from app.api.v1.users import router as users_router
 from app.api.v1.auth import router as auth_router
 from app.api.v1.profile import router as profile_router
 from app.api.v1.servers import router as servers_router
+from app.api.v1.plans import router as plans_router
 
 router = APIRouter()
 
@@ -11,3 +12,4 @@ router.include_router(users_router)
 router.include_router(auth_router)
 router.include_router(profile_router)
 router.include_router(servers_router)
+router.include_router(plans_router)
